d2b29daf2e18ab230cea0b4badba69c5fd4cb33f
Patrick Acker <patrickacker@gmail.com>
2016-06-22 15:26:34 -0400

    Add variable plaground area

---
+++ variable-playground.js
@@ -0,0 +1,28 @@
+// var person = {
+//   name: 'Andrew',
+//   age: 21
+// };
+//
+// function updatePerson(object) {
+//   // obj = {
+//   //   name: 'Andrew',
+//   //   age: '24'
+//   // };
+//   object.age = 24;
+//
+// }
+//
+// updatePerson(person);
+// console.log(person);
+
+// Array example
+var grades = [15, 37];
+
+function addGrade(array) {
+  array.push(55);
+  debugger;
+}
+
+addGrade(grades);
+console.log(grades);
+// push
