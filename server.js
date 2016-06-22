42df7cbdf86617b0ba519c8f902ea0d69feea2bd
Patrick Acker <patrickacker@gmail.com>
2016-06-22 00:04:53 -0400

    Added post routes

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-  id: 1,
-  description: 'Meet mom for lunch',
-  completed: false
-}, {
-  id: 2,
-  description: 'Go to market',
-  completed: false
-}, {
-  id: 3,
-  description: 'Finish this course',
-  completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+// whenever json request comes in, express parses this and we can
+// access via request.body
+app.use(bodyParser.json()); // helps to setup middleware
 
 app.get('/', function(req, res) {
   res.send('ToDo api root');
@@ -38,6 +32,15 @@ app.get('/todos/:id', function(req, res) {
   } else {
     res.status(404).send();
   }
+});
+
+// POST /todos
+app.post('/todos', function(req, res) {
+  var body = req.body;
+  body.id = todoNextId;
+  todoNextId++;
+  todos.push(body);
+  res.json(body);
 });
 
 
