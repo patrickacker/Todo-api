43d49653d4e2033acef2e9ddfe658c354e505a19
Patrick Acker <patrickacker@gmail.com>
2016-06-21 23:10:45 -0400

    Refactor server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-var PORT = process.env.port || 3000;
+var PORT = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
   res.send('ToDo api root');
